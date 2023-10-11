8a5186d3526723599af1e8415253256e28dd7b88
Vincent <51759317+Vincentvdt@users.noreply.github.com>
2023-10-11 11:48:25 +0200

    fetch photographer by id

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,2 +1,22 @@
 const params = new URL(document.location).searchParams;
 const id = params.get("id");
+
+const getPhotographer = async (id) => {
+  const response = await fetch("../../data/photographers.json");
+  const data = await response.json();
+
+  const photographer = data.photographers.find(
+    (photograph) => photograph.id === parseInt(id)
+  );
+
+  return photographer;
+};
+
+// Usage example:
+getPhotographer(id)
+  .then((photographer) => {
+    console.log(photographer);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
