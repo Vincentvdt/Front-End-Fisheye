239cc4f5b4a7bba0012c053454fd1abca151d28f
Vincent <51759317+Vincentvdt@users.noreply.github.com>
2023-10-12 17:46:50 +0200

    photographer header

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,3 +1,5 @@
+const mainDOM = document.querySelector("main");
+
 const params = new URL(document.location).searchParams;
 const id = params.get("id");
 
@@ -10,10 +12,27 @@ const getPhotographer = async (id) => {
   );
 };
 
-getPhotographer(id)
-  .then((photographer) => {
-    console.log(photographer);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+const populatePhotographHeader = (photographer) => {
+  const picture = `../assets/photographers/${photographer.portrait}`;
+
+  const headerElement = document.querySelector(".photograph-header");
+  const nameElem = document.querySelector(".photograph-header__name");
+  const locationElem = document.querySelector(".photograph-header__location");
+  const taglineElem = document.querySelector(".photograph-header__tagline");
+  const imgElem = document.querySelector(
+    ".photograph-header__profile-wrapper img"
+  );
+  headerElement.classList.remove("await");
+  nameElem.textContent = photographer.name;
+  locationElem.textContent = `${photographer.city},${photographer.country}`;
+  taglineElem.textContent = photographer.tagline;
+  imgElem.src = picture;
+  imgElem.alt = photographer.name;
+};
+const main = async () => {
+  const photographer = await getPhotographer(id);
+
+  populatePhotographHeader(photographer);
+};
+
+main();
