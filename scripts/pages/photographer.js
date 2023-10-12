9542300d7f172354c1912068f9d2138199255038
Vincent <51759317+Vincentvdt@users.noreply.github.com>
2023-10-12 21:51:38 +0200

    move pictures into a media folder

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -19,6 +19,33 @@ const getPhotographerMedia = async (id) => {
   return data.media.filter((media) => media.photographerId === parseInt(id));
 };
 
+const getGalleryCardDom = async (card) => {
+  const picture = `../../assets/medias/${card.image}`;
+
+  const htmlString = `
+            <article id="${card.photographerId}-${card.id}" class="gallery-card">
+            <a class="gallery-card__picture">
+              <img alt="${card.title}" src="${picture}">
+            </div>
+            <div class="gallery-card__description">
+              <h3 class="gallery-card__title">${card.title}</h3>
+              <div class="gallery-card__likes">
+                <p>${card.likes}</p>
+                <svg fill="none" height="19" viewBox="0 0 19 19" width="19" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      d="M9.5 18.85L8.23125 17.53C3.725 12.86 0.75 9.78 0.75 6C0.75 2.92 2.8675 0.5 5.5625 0.5C7.085 0.5 8.54625 1.31 9.5 2.59C10.4538 1.31 11.915 0.5 13.4375 0.5C16.1325 0.5 18.25 2.92 18.25 6C18.25 9.78 15.275 12.86 10.7687 17.54L9.5 18.85Z"
+                      fill="#911C1C"/>
+                </svg>
+              </div>
+            </a>
+          </article>
+  `;
+
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlString, "text/html");
+  return doc.body.firstChild;
+};
+
 const populatePhotographHeader = (photographer) => {
   const picture = `../assets/photographers/${photographer.portrait}`;
 
@@ -40,7 +67,10 @@ const main = async () => {
   const photographer = await getPhotographer(id);
   const medias = await getPhotographerMedia(id);
 
-  console.log(medias);
+  medias.forEach(async (media) => {
+    const cardHTML = await getGalleryCardDom(media);
+    mainDOM.append(cardHTML);
+  });
 
   populatePhotographHeader(photographer);
 };
