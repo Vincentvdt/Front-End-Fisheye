161189df1d4e1c6ee87e495ce91c2977ccaa5107
Vincent <51759317+Vincentvdt@users.noreply.github.com>
2023-10-28 17:33:42 +0200

    modal validate and close when click outside

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,30 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
-}
+const modalBackground = document.getElementById("contact_modal");
+const modal = document.querySelector(".modal");
+const modalForm = document.querySelector(".modalForm");
+const displayModal = () => {
+  modalBackground.style.display = "block";
+};
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
-}
+const closeModal = () => {
+  modalBackground.style.display = "none";
+};
+
+modalForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+const validate = (form) => {
+  const values = {};
+  for (const input of form) {
+    if (input.name) {
+      values[input.name] = input.value;
+    }
+  }
+  console.log(values);
+  closeModal();
+};
+
+document.addEventListener("click", (e) => {
+  if (e.target === modalBackground) {
+    closeModal();
+  }
+});
